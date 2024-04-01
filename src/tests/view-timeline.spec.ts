6e08c71d9a6a1f111fbd99aff4adab7648403d42
Nicolas Bernin <nicolas.bernin@reccat.io>
2024-04-02 00:29:36 +0200

    feat(posting-message.spec) add use case ViewTimelineUseCase working progress

---
+++ src/tests/view-timeline.spec.ts
@@ -0,0 +1,72 @@
+import {Message} from "../post-message.usecase";
+import {InMemoryMessageRepository} from "../message-inmemory.repository";
+
+describe("Feature: Viewing a personal timeline", () => {
+  let fixture: Fixture;
+
+  beforeAll(() => {
+    fixture = createFixture();
+  });
+
+  describe("Rule: Messages are shown in reverse chronological order", () => {
+    test("Alice can view the 2 messages she published in her timeline", async () => {
+      fixture.givenTheFollowingMessagesExist([
+        {
+          author: "Alice",
+          text: "My first message",
+          id: "message-1",
+          publishedAt: new Date("2023-02-07T16:28:00Z"),
+        },
+        {
+          author: "Bob",
+          text: "Hi it's Bob",
+          id: "message-2",
+          publishedAt: new Date("2023-02-07T16:32:00Z"),
+        },
+        {
+          author: "Alice",
+          text: "My second message",
+          id: "message-3",
+          publishedAt: new Date("2023-02-07T16:34:00Z"),
+        },
+      ]);
+
+      fixture.giveNowIs(new Date("2023-02-07T16:35:00Z"));
+
+      await fixture.whenUserseesTheTimelineOf("Alice");
+
+      fixture.thenUserShouldSee([
+        {
+          author: "Alice",
+          text: "My second message",
+          publishedTime: "1 minute ago",
+        },
+        {
+          author: "Alice",
+          text: "My first message",
+          publishedTime: "7 minutes ago",
+        },
+      ]);
+
+    });
+  });
+});
+
+const createFixture = () => {
+  let timeline: {
+    author: string,
+    text: string,
+    publishedTime: string
+  }[];
+  const messageRepository = new InMemoryMessageRepository();
+  return {
+    givenTheFollowingMessagesExist(messages: Message[]) {},
+    giveNowIs(now: Date) {},
+    async whenUserseesTheTimelineOf(user: string) {},
+    thenUserShouldSee(expectedTimeline: {author: string, text: string, publishedTime: string}[]) {
+      expect(timeline).toEqual(expectedTimeline);
+    }
+  }
+};
+
+type Fixture = ReturnType<typeof createFixture>;
